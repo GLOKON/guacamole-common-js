04a234312ae580c1aaafa085123a80fe1493b4ca
Daniel McAssey <daniel.mcassey@gmail.com>
2023-08-05 00:35:48 +0100

    ci: fix build

--- build.cjs
+++ build.cjs
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const srcDir = './src/';
 
-fs.rmSync('./lib/', { recursive: true, force: true });
-
 const src = fs.readdirSync(srcDir)
     .map(file => {
         const content = fs.readFileSync(srcDir + file, 'utf-8');
@@ -10,14 +8,9 @@ const src = fs.readdirSync(srcDir)
     })
     .join('\n\n');
 
-const esm = `${src}
-export default Guacamole;`;
-
-fs.mkdirSync('./lib/esm/', { recursive: true });
-fs.writeFileSync('./lib/esm/index.js', esm);
-
 const cjs = `${src}
 module.exports = Guacamole;`;
 
-fs.mkdirSync('./lib/cjs/', { recursive: true });
-fs.writeFileSync('./lib/cjs/index.js', cjs);
+fs.rmSync('./lib/', { recursive: true, force: true });
+fs.mkdirSync('./lib/', { recursive: true });
+fs.writeFileSync('./lib/index.js', cjs);
